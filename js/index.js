95cb02b0f12db69454763d048c445f962fdc8ce6
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2025-09-05 00:25:25 +0300

    enhancments

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
+import Question from "./question.js";
 import Quiz from "./quiz.js";
-
 // &-----HTML ELEMENTS----
 const categoryMenu = document.querySelector("#categoryMenu");
 const difficultyOptions = document.querySelector("#difficultyOptions");
 const questionsNumber = document.querySelector("#questionsNumber");
 const startQuizBtn = document.querySelector("#startQuiz");
 const quizOptions = document.querySelector("#quizOptions");
-const questionsContainer = document.querySelector(".questions-container");
+const appFooter= document.querySelector(".app-footer")
+export const questionsContainer = document.querySelector(
+  ".questions-container"
+);
 // ~-----GLOBAL VARIABLES----
+export let questions = [];
+export let currentQuiz = {};
 // *-----FUNCTIONS----
+
 // !-----EVENTS----
 startQuizBtn.addEventListener("click", async () => {
   const category = categoryMenu.value;
   const difficulty = difficultyOptions.value;
   const amount = questionsNumber.value;
-  const currentQuiz = new Quiz(difficulty, amount, category);
-  const questions = await currentQuiz.getQuestions();
+  currentQuiz = new Quiz(difficulty, amount, category);
+  questions = await currentQuiz.getQuestions();
   console.log({ questions: questions });
-  quizOptions.classList.add("d-none")
-  questionsContainer.classList.remove("d-none")
+  quizOptions.classList.add("d-none");
+  appFooter.classList.add("d-none");
+  questionsContainer.classList.remove("d-none");
+  let firstQuestion = new Question(0);
+  console.log(firstQuestion);
+  firstQuestion.displayQuestion();
 });
--- js/question.js
+++ js/question.js
@@ -1,5 +1,66 @@
-export default class Question{
-constructor(){
+import { currentQuiz, questions, questionsContainer } from "./index.js";
+let liElements;
+export default class Question {
+  constructor(index) {
+    this.index = index;
+    this.question = questions[index].question;
+    this.correct = questions[index].correct_answer;
+    this.wrongAnswers = questions[index].incorrect_answers;
+    this.category = questions[index].category;
+    this.allAnswers = this.concatWrongAndCorrectAnswers();
+    this.answered = false;
+  }
 
-}
+  concatWrongAndCorrectAnswers() {
+    return this.wrongAnswers.concat(this.correct).sort();
+  }
+
+  displayQuestion() {
+    questionsContainer.innerHTML += ` 
+      <div class="question shadow-lg col-lg-8 offset-lg-2 p-4 rounded-4">
+        <div class="question-header">
+          <span class="btn btn-category">${this.category}</span>
+          <span class="btn btn-questions">${this.index + 1} of ${
+      questions.length
+    } Questions</span>
+        </div>
+        
+        <h2 class="question-text">${this.question}</h2>
+        
+        <ul class="choices list-unstyled m-0">
+          ${this.allAnswers
+            .map(
+              (answer) => `
+            <li class="choice-item">${answer}</li>
+          `
+            )
+            .join("")}
+        </ul>
+        
+        <div class="score-display">
+          <h2 class="score-color">
+            Score: ${currentQuiz.score}
+          </h2>
+        </div>
+      </div>
+    `;
+    liElements = document.querySelectorAll(".choices li");
+    liElements.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        this.checkAnswer(e);
+      });
+    });
+  }
+
+  checkAnswer(e) {
+    if (!this.answered) {
+      this.answered = true;
+      if (this.correct === e.target.innerHTML) {
+        e.target.style.backgroundColor = "#4ade80";
+        currentQuiz.score++;
+      } else {
+        e.target.style.backgroundColor = "rgb(240, 30, 30)";
+      }
+    }
+  }
 }
